764416c1460ad50291eeddfa985988637ab77224
e-polischuk <z.polischuk@gmail.com>
2016-08-30 08:42:41 +0300

    Update Queens.java

--- Queens.java
+++ Queens.java
@@ -76,11 +76,11 @@ public class Queens {
 	}
 	
 	public int factor(int n) {
-        int f = 1;
-        for (int i = 2; i <= n; i++) f *= i;
-        return f;
-    }
-	
+        	int f = 1;
+        	for (int i = 2; i <= n; i++) f *= i;
+        	return f;
+    	}
+    	
 	public int[] getIndexArray(int n) {
 		int[] index = new int[n];
 		for(int i = 0; i < n; i++) index[i] = i;
